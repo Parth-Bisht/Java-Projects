a571f95d36db8d9085ee44cc4beeee4d39fa285f
Parth Bisht <bishtparth795@gmail.com>
2023-03-08 17:56:14 +0530

    fix(added data types)

---
+++ understnading variables/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int value1 =12, value2 = 14;
+        System.out.println(value1);
+    }
+}
---
+++ understnading variables/src/PrimitiveDataType.java
@@ -0,0 +1,29 @@
+public class PrimitiveDataType {
+    public static void main(String[] args){
+        int max = 2147483647; //max value for int data type
+        int min = -2147483648; //min value for int data type
+
+        short shortMax = 32767; //max value for short data type
+        short shortMin = -32768; //min value for short data type
+
+        long longMax = 9223372036854775807L; //use L in the end to make it long data type
+        long longMin = -9223372036854775808L;
+
+         byte byteMax = 127;
+         byte byteMin = -128;
+
+         float value2 = 9; //show 9.0 in the console, 32bit memory taken
+        double value3 = 9; //64bit memory
+
+        int marker = 512;
+        double percentage  = marker * 0.46f;
+
+
+        System.out.println("Percentage:"+percentage);
+
+
+        boolean var = false;
+        System.out.println(var);
+
+    }
+}
