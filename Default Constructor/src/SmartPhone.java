7a46b9141be0df9b7908081edbbaed88ef778182
Parth Bisht <bishtparth795@gmail.com>
2023-03-10 19:51:24 +0530

    classes

---
+++ Default Constructor/src/SmartPhone.java
@@ -0,0 +1,11 @@
+public class SmartPhone {
+    private String brand = "Samsung"; // if this is added than no need of constructor
+
+//    public SmartPhone(String brand){ //this is automatically by java is not manually added
+//        this.brand = brand;
+//    }
+
+    public String getBrand() {
+        return brand;
+    }
+}
