732742b08a37bf3d2340beb8f582ef813e8611f1
Parth Bisht <bishtparth795@gmail.com>
2023-03-20 11:58:03 +0530

    compariable interface

--- Compariable Interface/src/Main.java
+++ Compariable Interface/src/Main.java
@@ -1,5 +1,4 @@
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 class Names implements Comparable<Names>{
     private String name;
@@ -18,16 +17,39 @@ class Names implements Comparable<Names>{
             return 1;
         }
     }
+
+    @Override
+    public String toString() {
+        return "Names{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
 public class Main {
     public static void main(String[] args) {
-        String x = "a";
-        String y = "b";
-        x.compareTo(y);
+//        String x = "a";
+//        String y = "b";
+//        x.compareTo(y);
+        List<Object> elements = new LinkedList<>();
+        elements.add(new Names("Parth"));
+        elements.add(new Names("John"));
+        elements.add(new Names("Ed"));
+        elements.add(new Names("Mia"));
+        elements.add("Some string");
+        elements.add(1);
+        elements.add(2.0);
+        elements.add('#');
+
+        Main app = new Main();
+        app.printList(elements);
+
+
+//        Collections.sort(elements); //because of different data types it is not able to sort the data
+
     }
 
-    void printList(List<String> list){
-        ListIterator<String> iterator = list.listIterator();
+    void printList(List<Object> list){
+        ListIterator<Object> iterator = list.listIterator();
         while(iterator.hasNext()){
             System.out.println("Element "+iterator.next());
         }
