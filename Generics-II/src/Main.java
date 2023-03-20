51d39bd53fe5301111a55fadba98373dd27985e4
Parth Bisht <bishtparth795@gmail.com>
2023-03-20 13:00:47 +0530

    type parameters

--- Generics-II/src/Main.java
+++ Generics-II/src/Main.java
@@ -1,5 +1,53 @@
+import java.util.*;
+class Data<T>{
+    private T myVariable;
+
+    public Data(T myVariable) {
+        this.myVariable = myVariable;
+    }
+
+    public T getMyVariable() {
+        return myVariable;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "myVariable=" + myVariable +
+                '}';
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        List<Data<Object>> elements = new LinkedList<>();
+//        elements.add(new Data<Object>("Parth"));
+//        elements.add(new Data<Object>(1.0));
+//        elements.add(new Data<Object>(2));
+//        elements.add(new Data<Object>('%'));
+//        elements.add(new Data<Object>(5.0f));
+
+        //generic
+        List<Data<Integer>> elements = new LinkedList<>();
+        elements.add(new Data<Integer>(1));
+        elements.add(new Data<Integer>(2));
+        elements.add(new Data<Integer>(3));
+        elements.add(new Data<Integer>(4));
+        elements.add(new Data<Integer>(5));
+
+
+
+        Main app = new Main();
+        app.printList(elements);
+
+
+//        Collections.sort(elements); //because of different data types it is not able to sort the data
+
+    }
+
+    void printList(List<Data<Integer>> list){
+        ListIterator<Data<Integer>> iterator = list.listIterator();
+        while(iterator.hasNext()){
+            System.out.println("Element "+iterator.next().getMyVariable());
+        }
     }
 }
---
+++ Type parameters/src/Main.java
@@ -0,0 +1,45 @@
+/*
+The naming convention are as follows:
+
+T - Type
+E - Element
+K - Key
+N - Number
+V - Value
+ */
+
+class Data<K,V>{
+    private K key;
+    private V value;
+
+    public Data(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+    public<E,N> void display(E element,N number){
+        System.out.println("Element: "+element+" Number: "+number);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Data data = new Data(1,"Parth");
+        System.out.println("Key: "+data.getKey()+" value: "+data.getValue());
+        data.display("element",2);
+    }
+}
