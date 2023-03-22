7c1c28bd5dfe3778332e5bb05a9fc24631891a8c
Parth Bisht <bishtparth795@gmail.com>
2023-03-22 12:44:17 +0530

    reading file using scanner class

---
+++ read file using scanner/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(new File("test.txt"));
+            String line;
+            while(sc.hasNext()){
+                line = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error while reading file");
+        }
+    }
+}
