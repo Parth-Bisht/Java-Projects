711c37760be92af7837320f9322eb94e35f5bb9b
Parth Bisht <bishtparth795@gmail.com>
2023-03-22 12:00:58 +0530

    Predicates

--- Predicates/src/Main.java
+++ Predicates/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.function.IntPredicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        IntPredicate lessthan18 = new IntPredicate() {
+//            @Override
+//            public boolean test(int value) {
+//                if(value<18) return true;
+//                else return false;
+//            }
+//        };
+
+        IntPredicate lessThan18 = i -> i<18;
+        IntPredicate moreThan10 = i -> i>10;
+//        System.out.println(lessthan18.and(morethan10).test(20));
+
+        new Main().demo(11,lessThan18,moreThan10);
+    }
+
+    public void demo(int x,IntPredicate lessThan18, IntPredicate moreThan10){
+        if(lessThan18.and(moreThan10).test(x)){
+            System.out.println("The input is less than18 and more than 10");
+        }else{
+            System.out.println("Invalid input");
+        }
     }
 }
