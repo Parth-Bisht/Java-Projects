b7faaeffbe9d91072a2620058c7516a3d6de2991
Parth Bisht <bishtparth795@gmail.com>
2023-03-23 16:04:34 +0530

    saving image to database

--- projectWithMaven/src/main/java/org/parth/projectWithMaven/App.java
+++ projectWithMaven/src/main/java/org/parth/projectWithMaven/App.java
@@ -1,5 +1,10 @@
 package org.parth.projectWithMaven;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,20 +14,37 @@ import org.hibernate.cfg.Configuration;
  *
  */
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Project Started");
 
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 
 		// creating student
-		Student st = new Student(2, "saurav", "hyd");
-
+		Student st = new Student(1, "parth", "hld");
+		
+		//creating object of address class		
+		Address ad = new Address();
+		ad.setStreet("street1");
+		ad.setCity("DELHI");
+		ad.setOpen(true);
+		ad.setAddedDate(new Date());
+		ad.setX(12.2343434d);
+		
+		//reading image
+		FileInputStream fis = new FileInputStream("src/main/java/pic.png");
+		byte[] data = new byte[fis.available()];
+		fis.read();
+		ad.setImage(data);		
+		
+		//opening session
 		Session session = factory.openSession();
 
 		session.beginTransaction();
 		session.save(st);
+		session.save(ad);
 		session.getTransaction().commit();
 
 		session.close();
+		System.out.println("Done!");
 	}
 }
