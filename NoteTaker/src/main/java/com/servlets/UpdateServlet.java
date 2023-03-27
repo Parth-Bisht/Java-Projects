75f469c62634b224ec4f3f9e169876e13aac5d18
Parth Bisht <bishtparth795@gmail.com>
2023-03-27 16:33:05 +0530

    note taker completed

--- NoteTaker/src/main/java/com/servlets/UpdateServlet.java
+++ NoteTaker/src/main/java/com/servlets/UpdateServlet.java
@@ -1,7 +1,6 @@
 package com.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -15,35 +14,33 @@ import org.hibernate.Transaction;
 import com.entities.Note;
 import com.helper.FactoryProvider;
 
-
-public class SaveNoteServlet extends HttpServlet {
+public class UpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public SaveNoteServlet() {
+    public UpdateServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
-    
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-    	response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		try {
 			String title = request.getParameter("title");
 			String content = request.getParameter("content");
-			
-			Note note = new Note( title, content, new Date());
+			int noteId = Integer.parseInt(request.getParameter("noteId").trim());
 			
 			Session s = FactoryProvider.getFactory().openSession();
 			Transaction tx = s.beginTransaction();
-			s.save(note);
+			
+			Note note = s.get(Note.class, noteId);
+			note.setTitle(title);
+			note.setContent(content);
+			note.setAddedDate(new Date());
+			
 			tx.commit();
 			s.close();
-			response.setContentType("text/html");
-			PrintWriter out = response.getWriter();
-			out.println("<h1 style='text-align:center;'> Note added successfully </h1>");
-			out.println("<h1 style='text-align:center;'> <a href='all_notes.jsp'>View all Notes</a></h1>");
+			
+			response.sendRedirect("all_notes.jsp");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
