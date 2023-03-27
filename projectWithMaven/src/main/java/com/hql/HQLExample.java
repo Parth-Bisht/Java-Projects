c5d1074af4b88b32e752b3116cb3cbc4472c2fa6
Parth Bisht <bishtparth795@gmail.com>
2023-03-27 20:04:50 +0530

    spring

--- projectWithMaven/src/main/java/com/hql/HQLExample.java
+++ projectWithMaven/src/main/java/com/hql/HQLExample.java
@@ -1,10 +1,12 @@
 package com.hql;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.parth.projectWithMaven.Student;
 
@@ -25,6 +27,28 @@ public class HQLExample {
 		
 		for(Student student: list) {
 			System.out.println(student.getName());
+		}
+		
+		System.out.println("_________________________");
+		
+		Transaction tx = session.beginTransaction();
+//		Query q2 = session.createQuery("delete from Student as s where s.city=:c");
+//		q2.setParameter("c", "ntl");
+//		int r = q2.executeUpdate();
+//		System.out.println("Deleted: ");
+		
+//		Query q2 = session.createQuery("update Student set city=:c where name=:n");
+//		q2.setParameter("c", "delhi");
+//		q2.setParameter("n", "naruto");
+//		int r = q2.executeUpdate();
+//		System.out.println(r+"objects updated");
+			
+		tx.commit();
+		
+		Query q3 = session.createQuery("select q.question,q.questionId,a.answer from Question as q INNER JOIN q.answers as a");
+		List<Object []> list3 = q3.getResultList();
+		for(Object[] arr: list3) {
+			System.out.println(Arrays.toString(arr));
 		}
 		
 		session.close();
