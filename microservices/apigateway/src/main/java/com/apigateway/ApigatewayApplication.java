e14edfa53e4497cd1782232a772fee0b242b2249
Parth Bisht <bishtparth795@gmail.com>
2023-03-29 19:53:20 +0530

    hystrix

--- microservices/apigateway/src/main/java/com/apigateway/ApigatewayApplication.java
+++ microservices/apigateway/src/main/java/com/apigateway/ApigatewayApplication.java
@@ -2,7 +2,10 @@ package com.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
 @SpringBootApplication
+@EnableHystrix
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
